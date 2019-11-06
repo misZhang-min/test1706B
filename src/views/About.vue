6f29176a724ef9c50c41af049d853a97c73df9c0
Zhang Min <1481082204@qq.com>
2019-11-06 09:08:18 +0800

    day4

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+       arr:[1,2,3,4,'a']
+    }
+  },
+  created() {
+    this.fun()
+  },
+  methods: {
+    fun(){
+      this.arr.forEach((item)=>{
+        return console.log(item)
+      })
+    }
+  },
+}
+</script>
